7f4aca2204327e053479857953e41a5713d58af0
BlackHawk1665 <70489197+BlackHawk1665@users.noreply.github.com>
2020-09-07 23:05:11 -0400

    Add files via upload

---
+++ CH1Homework.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import pandas as pd
+Location = "C:\Resume\Florida_TP.csv"
+df = pd.read_csv(Location,header=None)
+                 
+               
+
+
+# In[20]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
