f7ccf866aaff736e9636a77272ecfdc2480b02b8
BlackHawk1665 <70489197+BlackHawk1665@users.noreply.github.com>
2020-09-07 23:43:33 -0400

    Add files via upload

---
+++ CH1.2HW.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+
+import pandas as pd
+Location = "C:\Resume\AGE.xls"
+df = pd.read_excel(Location)
+
+
+# In[7]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
